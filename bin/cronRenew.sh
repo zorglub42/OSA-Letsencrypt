7cf603d2bb03a31bd4df2b0c1604c5638c839069
jmjb0521 <contact@zorglub42.fr>
2017-03-04 17:11:18 +0100

    cronRenew bug fix

--- bin/cronRenew.sh
+++ bin/cronRenew.sh
@@ -30,7 +30,7 @@ cd `dirname $0`
 	for n in `ls ../data/` ; do
 		curl -i -s -k --user "$OSA_USAGE_USER:$OSA_ADMIN_PWD"  $OSA_LOCAL_SERVER/ApplianceManager/nodes/$n | grep "404 Not Found">/dev/null
 		if [ $? -eq 0 ] ; then
-			 #./revokeCerts.sh $n
+			./revokeCerts.sh $n
 			echo revoking certs for $n
 			rm ../data/$n
 		else		
